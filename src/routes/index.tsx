a9c51994c285f33ab610f654dbde6a5d25dcd9b1
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-01-10 14:51:05 +0200

    style: formatted code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,20 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hallo, Qwik qorld!</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>Hallo, Qwik qorld!</h1>
+        </>
+    );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+    title: 'MiniYT',
+    meta: [
+        {
+            name: 'description',
+            content: 'Qwik site description',
+        },
+    ],
 };
